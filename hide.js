5ed2d481eb7a6ed33155b7c9a9e60b8bb34256ce
Luiz Ricardo <luizrgfelipe@gmail.com>
2021-08-20 16:48:34 -0300

    modulo de se esconder finalizado

---
+++ hide.js
@@ -0,0 +1,66 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+async function requestStop(){
+
+    const promisse = new Promise((resolve,reject)=>{
+
+        try{
+
+            page.on('request',request=>{
+        
+                if(request.url().includes('/seen/')){
+                    request.abort()
+                    resolve(true)
+                }else{
+                    request.continue()
+                }
+        
+            })
+        }catch{
+            reject(false)
+        }
+
+    })
+}
+
+
+
+
+
+async function hideVisu(){
+
+    const browser = await puppeteer.launch({
+        headless:false
+    })
+    const page = await browser.newPage()
+
+    page.setRequestInterception(true)
+
+    //analisando request e bloqeuando
+    page.on('request',request=>{
+        
+        if(request.url().includes('/seen/')){
+            request.abort()
+            resolve(true)
+        }else{
+            request.continue()
+        }
+
+    })
+
+    //carregando página do instagram
+    await page.goto('https://www.instagram.com/')
+    //pegando cookies
+    const cookies = JSON.parse(fs.readFileSync('./cookies.json','utf-8'))
+    //setando os cookies na página
+    await page.setCookie(...cookies)
+    await page.reload()
+    await page.goto('https://www.instagram.com/direct/inbox/')
+    
+
+}
+
+
+
+hideVisu()
